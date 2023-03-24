a760be277931592b2ad2c91d98ea6d3ae5cc23f0
C P Singh <120619302+CPatgithub@users.noreply.github.com>
2023-03-25 01:50:02 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -3,10 +3,10 @@ import ListRender from "./ListRender";
 
 const Today = (props) => {
   const date = new Date();
-  
+  const list = props.list.filter((obj)=>obj.date == date.toLocaleDateString("en-US"));
   return (
     <div id="today-list">
-      <ListRender list={} />
+      <ListRender list={list} />
     </div>
   );
 };
